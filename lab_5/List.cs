457a45ab7030ea6fe67f69ec2f3fd7df58b0168f
Arthur <shporgalkanope@gmail.com>
2024-11-24 14:32:07 +0700

    Generic type update
    1. Generic type implementation
    2. IEnumerator interface implementation

--- lab_5/List.cs
+++ lab_5/List.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.Serialization;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace lab_5
 {
-    internal class CList<T> where T : IComparable
+    internal class CList<T> : IEnumerable where T : IComparable
     {
         private Node<T>[] nodes;
         public Node<T> this[int index]
         {
-            get { return nodes[index]; }
-            set { nodes[index] = value; }
+            get 
+            { 
+                return nodes[index]; 
+            }
+            set 
+            {
+                Node<T> node = head;
+                if (index == 0)
+                {
+                    head = value;
+                    return;
+                }
+                for(int i = 0; i < index-1; i++)
+                {
+                    node = node.next;
+                }
+                node.next = value;
+            }
         }
 
         public Node<T> head;
@@ -29,22 +49,48 @@ namespace lab_5
 
         public void Push(T data)
         {
-            if (head == null) { head = new Node<T>(data); }
+            Node<T> current = head;
+            if (head == null) 
+            { 
+                head = new Node<T>(data);
+                nodes[count] = head;
+                count++;
+                return;
+            }
             Node<T> newNode = new Node<T>(data);
-            newNode.next = head;
-            head = newNode;
+            while(current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+            nodes[count] = newNode;
             count++;
         }
         public Node<T> Pop()
         {
+            Node<T> toReturn;
+            if (head.next == null)
+            {
+                toReturn = head;
+                head = null;
+                count = 0;
+                nodes[count] = null;
+                return toReturn;
+            }
             Node<T> headNode = head;
-            head = head.next;
+            while(headNode.next.next != null)
+            {
+                headNode = headNode.next;
+            }
+            toReturn = headNode.next;
             headNode.next = null;
             count--;
-            return headNode;
+            nodes[count] = null;
+            return toReturn;
         }
         public void Check()
         {
+            if (head == null) { return; }
             Node<T> current = head;
             do
             {
@@ -53,17 +99,44 @@ namespace lab_5
             }
             while (current != null);
         }
+        public Node<T> Find(T key)
+        {
+            Node<T> current = head;
+            while (current != null && current.data.CompareTo(key) != 0)
+            {
+                current = current.next;
+            }
+            return current;
+        }
 
+        public IEnumerator GetEnumerator()
+        {
+            return new ListEnum(nodes);
+        }
 
+        private class ListEnum : IEnumerator
+        {
+            private Node<T>[] collection;
 
-        //public Node<T> Find(T key)
-        //{
-        //    Node<T> current = head;
-        //    while (current.data == key)
-        //    {
-        //        current = current.next;
-        //    }
-        //    return current;
-        //}
+            private int position = -1;
+            
+            public ListEnum(Node<T>[] nodes)
+            {
+                collection = nodes;
+            }
+
+            public bool MoveNext()
+            {
+                position++;
+                return (position < collection.Length);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            public object Current => collection[position];
+        }
     }
 }
--- lab_5/Program.cs
+++ lab_5/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace lab_5
+﻿using System.Net.Sockets;
+
+namespace lab_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CList<string> strList = new CList<string>(3);
+            strList.Push("Atmos");
+            strList.Push("Bell");
+            strList.Push("Cypher");
+            
+            foreach(Node<string> node in strList)
+            {
+                Console.WriteLine(node.data);
+            }
         }
     }
 }
